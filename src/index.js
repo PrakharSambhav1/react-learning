33756bd8bc6ad97b3d51210399f8c54662d41735
root <root@DESKTOP-D5KUQAN.localdomain>
2020-03-29 18:04:18 +0530

    start and jsx object

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const element = <h1>HAlo world</h1>;
+console.log(element); //logs the react obj on console.
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Function renders the react jsx object in the root element of the public/index.html
+ReactDOM.render(element, document.getElementById("root"));
