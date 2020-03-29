8e2663883ea1ebc1b74170c00c32e36de82afcf6
root <root@DESKTOP-D5KUQAN.localdomain>
2020-03-29 18:15:16 +0530

    basic

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const element = <h1>HAlo world</h1>;
-console.log(element); //logs the react obj on console.
+// console.log(element); //logs the react obj on console.
 
 //Function renders the react jsx object in the root element of the public/index.html
 ReactDOM.render(element, document.getElementById("root"));
